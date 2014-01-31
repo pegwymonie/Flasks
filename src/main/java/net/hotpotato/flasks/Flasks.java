79460c1d2e39483b4b8ed678aa6139725b9930df
pegwymonie <szurborg@yahoo.com>
2014-01-30 21:27:17 -0600

    initial

---
+++ src/main/java/net/hotpotato/flasks/Flasks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.hotpotato.flasks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Ohioyu
+ */
+public final class Flasks extends JavaPlugin {
+    
+     @Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+    
+}
